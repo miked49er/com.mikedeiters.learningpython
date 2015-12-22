806b1f0350e29af281a2c25adf2d5d435a1317fc
Mike Deiters <miked49er@gmail.com>
2015-12-21 21:48:38 -0500

    Chapter 12 Finished

--- 12 Classes/classes.py
+++ 12 Classes/classes.py
@@ -3,16 +3,65 @@
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
-class Duck:
+class Animal:
+    def talk(self):print('I have something to say')
+    def walk(self):print("Hey I'm walking here")
+    def clothes(self):print('I have nice close')
+
+class Duck(Animal):
+    def __init__(self, **kwargs):
+        self._variables = kwargs
+
     def quack(self):
         print('Quaaack!')
 
     def walk(self):
+        # super().walk()
         print('Walks like a duck.')
+
+    def bark(self):print('A Duck cannot bark')
+
+    def fur(self):print('A Duck has feathers')
+
+    # def get_color(self):
+    #     return self.variables.get('color', None)
+    #
+    # def set_color(self, color):
+    #     self.variables['color'] = color
+
+    def set_variable(self, key, value):
+        self._variables[key] = value
+
+    def get_variable(self, key):
+        return self._variables.get(key)
+
+class Dog(Animal):
+    def fur(self):print('I have brown and white fur')
+
+    def bark(self):print('Woof!')
+
+    def walk(self):print('Walks like a dog')
+
+    def quack(self):print('A dog cannot quack')
+
+def in_the_forest(dog):
+    dog.bark()
+    dog.fur()
+
+def in_the_pond(duck):
+    duck.quack()
+    duck.walk()
 
 def main():
     donald = Duck()
-    donald.quack()
-    donald.walk()
+    donald.set_variable('feet', 2)
+    donald.set_variable('color', 'blue')
+    # print('Color', donald.get_variable('color'))
+    # print('# of Feet', donald.get_variable('feet'))
+    fido = Dog()
+
+    for o in (donald, fido):
+        in_the_forest(o)
+        in_the_pond(o)
 
 if __name__ == "__main__": main()
--- 12 Classes/generator.py
+++ 12 Classes/generator.py
@@ -3,8 +3,32 @@
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
+class inclusive_range:
+    def __init__(self, *args):
+        numargs = len(args)
+        if numargs < 1:
+            raise TypeError('requires at least one argument')
+        elif numargs == 1:
+            self.start, self.step = 0, 1
+            self.stop = args[0]
+        elif numargs == 2:
+            (self.start, self.stop) = args
+            self.step = 1
+        elif numargs == 3:
+            (self.start, self.stop, self.step) = args
+        else:
+            raise TypeError('inclusive range expected at most 3 arguments, got {}'.format(numargs))
+
+    def __iter__(self): # Allows the object to be iterable without having to call an iter function
+        i = self.start
+        while i <= self.stop:
+            yield i
+            i += self.step
+
+
 def main():
-    o = range(25)
-    for i in o: print(i, end = ' ')
+    o = inclusive_range(25)
+    for i in o: print(i, end=' ')
+
 
 if __name__ == "__main__": main()
