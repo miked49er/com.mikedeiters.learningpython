cc83caab0660b18f150ae4b2d61ead9cb82e96ef
Mike Deiters <miked49er@gmail.com>
2015-12-21 20:24:40 -0500

    Chapter 11 Finished

--- 11 Functions/functions.py
+++ 11 Functions/functions.py
@@ -4,9 +4,21 @@
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
-    testfunc()
+    print(testfunc(42))
+    # testFunc2(42, 69, 101, 32, 21, 90)
+    # testFunc3(one=1, two=2, four=42)
 
-def testfunc():
-    print('This is a test function')
+def testfunc(num, another = None): # None is a default value that is basically null
+    return 'This is a test function'
+    # pass # for place holder functions
+
+def testFunc2(this, that, other, *args):
+    print(this, that, other)
+    for i in args: print(i, end='\n')
+
+def testFunc3(**kwargs):
+    # print(kwargs['one'], kwargs['two'], kwargs['four'])
+    for i in kwargs:
+        print(i, kwargs[i])
 
 if __name__ == "__main__": main()
--- 11 Functions/generator.py
+++ 11 Functions/generator.py
@@ -5,7 +5,27 @@
 
 def main():
     print("This is the functions.py file.")
-    for i in range(25):
-        print(i, end = ' ')
+    for i in inclusive_range():
+        print(i, end=' ')
+
+
+def inclusive_range(*args):
+    numargs = len(args)
+    if numargs < 1: raise TypeError('requires at least 1 argument')
+    elif numargs == 1:
+        stop = args[0]
+        start, step = 0, 1
+    elif numargs == 2:
+        (start, stop) = args
+        step = 1
+    elif numargs == 3:
+        (start, stop, step) = args
+    else:
+        raise TypeError('inclusive_range expected at most 3 arguments, got {}'.format(numargs))
+    i = start
+    while i <= stop:
+        yield i
+        i += step
+
 
 if __name__ == "__main__": main()
