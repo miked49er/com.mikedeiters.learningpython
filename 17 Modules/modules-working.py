e836e8ef8ad8cdc44b02531b2a4a91b9bae54d09
Mike Deiters <miked49er@gmail.com>
2015-12-23 10:27:35 -0500

    Chapter 17 Finished

---
+++ 17 Modules/modules-working.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# modules.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+import bitstring
+
+def main():
+   a = bitstring.BitString(bin='01010101')
+   print('0x{}'.format(a.hex), a.bin, a.uint)
+
+if __name__ == "__main__": main()
--- 17 Modules/modules.py
+++ 17 Modules/modules.py
@@ -7,5 +7,32 @@ import sys
 
 def main():
     print('Python version {}.{}.{}'.format(*sys.version_info))
+    print(sys.platform)
+
+    # import os
+    # print(os.name)
+    # print(os.getenv('PATH'))
+    # print(os.getcwd())
+    # print(os.urandom(25))
+
+    # import urllib.request
+    # page = urllib.request.urlopen('http://bw.org/')
+    # for line in page: print(str(line, encoding='utf_8'), end='')
+
+    # import random
+    # print(random.randint(1,1000))
+    # x=list(range(25))
+    # print(x)
+    # random.shuffle(x)
+    # print(x)
+    # random.shuffle(x)
+    # print(x)
+    # random.shuffle(x)
+    # print(x)
+
+    # import datetime
+    # now = datetime.datetime.now()
+    # print(now)
+    # print(now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond)
 
 if __name__ == "__main__": main()
