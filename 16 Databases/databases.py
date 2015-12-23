03889d3c50b584d878b0087bdf97407f1f3141c4
Mike Deiters <miked49er@gmail.com>
2015-12-23 09:41:32 -0500

    Chapter 16 Finished

--- 16 Databases/databases.py
+++ 16 Databases/databases.py
@@ -6,6 +6,17 @@
 import sqlite3
 
 def main():
-    print('This is the databases.py file')
+    db = sqlite3.connect('test.db')
+    db.row_factory = sqlite3.Row
+    db.execute('drop table if exists test')
+    db.execute('create table test (t1 text, i1 int)')
+    db.execute('insert into test (t1, i1) values (?, ?)', ('one', 1))
+    db.execute('insert into test (t1, i1) values (?, ?)', ('two', 2))
+    db.execute('insert into test (t1, i1) values (?, ?)', ('three', 3))
+    db.execute('insert into test (t1, i1) values (?, ?)', ('four', 4))
+    db.commit()
+    cursor = db.execute('select i1, t1 from test order by i1')
+    for row in cursor:
+        print(row['t1'], row['i1'])
 
 if __name__ == "__main__": main()
